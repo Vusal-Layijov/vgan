7fcd4574f1e1a4a812f36a891626243e4d69ac88
Vusal Layijov <Layijovvusal@gmail.com>
2023-08-18 18:30:14 -0700

    check

--- app/models/business.py
+++ app/models/business.py
@@ -1,6 +1,6 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import datetime
-# from models import business_category
+#check
 from .business_category import business_categories
 
 class Business(db.Model):
